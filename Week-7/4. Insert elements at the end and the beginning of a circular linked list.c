14fe05296f330a840754d7e4804795237adfeaf9
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-08-26 15:03:26 +0530

    Update 4. Insert elements at the end and the beginning of a circular linked list.c

--- Week-7/4. Insert elements at the end and the beginning of a circular linked list.c
+++ Week-7/4. Insert elements at the end and the beginning of a circular linked list.c
@@ -1,48 +1,4 @@
-[13:03, 8/26/2020] Sanchari CST UEM: #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-struct node
-{
-    int data;
-    struct node *link1;
-    struct node *link2;
-};
-int main()
-{
-    int i,pos,l;
-    char a[50];
-    scanf("%d",&pos);
-    getchar();
-    fgets(a,50,stdin);
-    l=(strlen(a)/2);
-    int ar[l];
-    int dd=0,r;
-    for(i=0;i<strlen(a);i++)
-    {
-        if((int)a[i]>=48)
-        {
-            r=(int)a[i]-48;
-            ar[dd++]=r;
-        }
-    }
-    l++;
-    struct node *p,*h;
-    p=h=(struct node*)malloc(sizeof(struct node));
-    p->link1=NULL;
-    p->data=ar[0];
-    for(i=1;i<l;i++)
-    {
-        p->link2=(struct node*)malloc(sizeof(struct node));
-        p=p->link2;
-        p->data=ar[i];
-    }
-    p->link2=NULL;
-    p=h;
-    int c=1;
-    if(pos==1)
-    {
-        p=p->link…
-[13:03, 8/26/2020] Sanchari CST UEM: #include<stdio.h>
+#include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 struct node
