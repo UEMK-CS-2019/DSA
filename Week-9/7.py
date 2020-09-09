7f3b7ffda28a6708ef1f9961d343d19f90f758bb
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-09-09 11:39:22 +0530

    Update and rename 7. to 7.py

---
+++ Week-9/7.py
@@ -0,0 +1,14 @@
+import json
+
+str1 = input()
+str2 = input()
+str3 = input()
+
+st1 = eval(str1)
+st2 = eval(str2)
+st3 = eval(str3)
+st = {}
+st.update(st1)
+st.update(st2)
+st.update(st3)
+print (st)
